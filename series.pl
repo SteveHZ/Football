13f5c3e9b6409130c844f6d8d615b0040675ff3c
SteveHZ <stevehope1@sky.com>
2020-09-12 11:27:45 +0100

    v3.12 120920

--- series.pl
+++ series.pl
@@ -1,18 +1,52 @@
 
-# series.pl 28/09/20
+# series.pl 28/08/20 - 01/09/20
 
 use MyHeader;
+use File::Find;
+use Football::Spreadsheets::Write_Series;
 
-my $line;
-my $page = 0;
-my $hash = {};
-my @pages = ("Wins", "Home Wins", "Away Wins", "Draws", "Home Draws", "Away Draws");
+my $path = "c:/mine/lisp/data";
+my @pages = ("Wins", "Home Wins", "Away Wins",
+			 "Draws", "Home Draws", "Away Draws",
+			 "Defeats", "Home Defeats", "Away Defeats");
 
-open my $fh, '<','c:/mine/lisp/data/series.csv';
-while ($line = <$fh>) {
-	$line =~ s/","/"\n"/g;  # change commas in between closing and opening quotes into new lines
-	$line =~ s/"//g;        # now remove quotes
-	$hash->{$pages [$page ++]} = [ split "\n", $line ];
+my $files = get_files ($path);
+
+for my $file (@$files) {
+	my $hash = {};
+	my $page = 0;
+
+	open my $fh, '<', "$path/$file" or die "Can't open $file";
+	while (my $line = <$fh>) {
+		chomp $line;
+
+		if ($line ne "") {
+			push $hash->{ $pages [$page] }->@*, $line;
+		} else {
+			$page ++;
+		}
+	}
+	close $fh;
+
+	my ($name, $junk) = split '\.', $file;
+	my $xlsx_filename = "C:/Mine/perl/Football/reports/$name.xlsx";
+	print "\nWriting $xlsx_filename...";
+	my $writer = Football::Spreadsheets::Write_Series->new (filename    => $xlsx_filename,
+															sheet_names => \@pages);
+	$writer->write ($hash);
+	print "Done";
 }
 
-print Dumper $hash;
+
+sub get_files {
+	my $dir = shift;
+	my @files = ();
+
+	find (
+	  sub {
+		  push @files, $_ if $_ =~ /series.*\.csv/;
+	  }, $dir
+    );
+    return \@files;
+}
+
